cac5b3effba6403d9a7a2684a2d1181a26f6185c
henrikdamdk <henrik@bettercollective.com>
2015-09-25 10:29:38 +0200

    First function

---
+++ warpscan.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Henrik
+ * Date: 25-09-2015
+ * Time: 10:06
+ */
+
+class warpscan {
+
+    public function getItem() {
+        return 'FIRSTitem!!!';
+    }
+
+}
